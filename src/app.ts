25f4ba763d6202fc81f9dc45d32b2bebcf64bdf9
ABU SAYED <farabisayed6099@gmail.com>
2023-11-17 21:23:16 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req: Request, res: Response) => {
+  const a = 'hello world'
+  res.send(a)
+})
+
+export default app
